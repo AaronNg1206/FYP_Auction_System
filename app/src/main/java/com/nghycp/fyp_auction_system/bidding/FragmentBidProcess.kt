c6739035bf8b0015a8fe4eaadf914422cd1a7c97
Aaron <zonghao6021@gmail.com>
2023-02-03 23:26:12 +0800

    FirstPage AppIcon

--- app/src/main/java/com/nghycp/fyp_auction_system/bidding/FragmentBidProcess.kt
+++ app/src/main/java/com/nghycp/fyp_auction_system/bidding/FragmentBidProcess.kt
@@ -1,10 +1,11 @@
-package com.nghycp.fyp_auction_system
+package com.nghycp.fyp_auction_system.bidding
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nghycp.fyp_auction_system.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentUserProfile.newInstance] factory method to
+ * Use the [FragmentBidProcess.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentUserProfile : Fragment() {
+class FragmentBidProcess : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class FragmentUserProfile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+        return inflater.inflate(R.layout.fragment_bid_process, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class FragmentUserProfile : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentUserProfile.
+         * @return A new instance of fragment FragmentBidProcess.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentUserProfile().apply {
+            FragmentBidProcess().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/nghycp/fyp_auction_system/usermanagement/FragmentRegister.kt
+++ app/src/main/java/com/nghycp/fyp_auction_system/usermanagement/FragmentRegister.kt
@@ -1,4 +1,4 @@
-package com.nghycp.fyp_auction_system
+package com.nghycp.fyp_auction_system.usermanagement
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.nghycp.fyp_auction_system.R
 import com.nghycp.fyp_auction_system.databinding.FragmentRegisterBinding
 
 /**
